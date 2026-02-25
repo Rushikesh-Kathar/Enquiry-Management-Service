2b10dacdc827cb85f3c1d40f3a1787d532d11dd4
Rushikesh Kathar <Rushikesh.Kathar@INFOVISIONLABS.COM>
2026-02-25 13:26:22 +0530

    Added services, controller and routes files

---
+++ src/routes/enquiry.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { enquiryController, getEnquiriesController, getCarModelsController, getEnquiryByIdController, getEnquiryBySearchController, updateEnquiryController } from '../controllers/enquiry.controller';
+
+const router = Router();
+router.route('/enquiry').post(enquiryController);
+router.route('/enquiries').get(getEnquiriesController);
+router.route('/car-models').get(getCarModelsController);
+router.route('/enquiries/search').get(getEnquiryBySearchController)
+router.route('/enquiries/:id').get(getEnquiryByIdController);
+router.route('/enquiry/:id').patch(updateEnquiryController);
+export default router;
